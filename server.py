e063cfda84998260b6d45f74f1212e7ed8c0dead
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-24 10:23:22 +0530

    Server Module
    This is the main server module

---
+++ server.py
@@ -0,0 +1,50 @@
+import socket as sc
+import select
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+server_socket.setsockopt(sc.SOL_SOCKET, sc.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+
+
+def rec_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode("utf-8").strip())
+
+        return {"header": message_header, "data": client_socket.recv(message_length)}
+
+
+    except:
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
