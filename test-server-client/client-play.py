4acc22117934314479a2a085830f9df877a88d49
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-24 11:31:12 +0530

    Update client
    Making the client be more dynamic in the way it sends the data by adding an header so that the server knows how much data is incoming or what the buffer size should be.

--- test-server-client/client-play.py
+++ test-server-client/client-play.py
@@ -1,8 +1,28 @@
 import socket as soc
 
-s = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
-s.connect((soc.gethostname(), 1234))
+HEADERSIZE = 10
 
-# 1024 decides the amount of data that you can receive at a time (Buffer Size)
-msg = s.recv(0)
-print(msg.decode('utf-8'))
+# Socket -> Socket is an endpoint(at an ip on a port) that sends and receives data
+# Initialize socket with socket family type AF_INET -> IPV4 & SOCK_STREAM -> streaming socket
+s = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
+s.connect(soc.gethostname(), 3200)
+
+# 1024 decides the amount of data that you can receive at a time
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f'new message length: {msg[:HEADERSIZE]}')
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+    if len(full_msg)-HEADERSIZE == msglen:
+        print('Full msg received!')
+        print(full_msg[HEADERSIZE:])
+        new_msg = True
+        full_msg = ''
+
+print(full_msg)
