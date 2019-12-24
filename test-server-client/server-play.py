86a8ad746ce8c67a326034630e341e3062b61aca
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-24 11:32:39 +0530

    enhance server's capability
    Adding extra configuration to the server

--- test-server-client/server-play.py
+++ test-server-client/server-play.py
@@ -1,20 +1,29 @@
 import socket as soc
+import time
 
-###############################################################################################
-# Socket -> It is an endpoint(at an ip on a port addr) that sends and receives messages or data
-###############################################################################################
+HEADERSIZE = 10
 
-# Initialize the socket object with socket family type: AF_INET -> IPv4 & SOCK_STREAM -> streaming socket
+# Socket -> Socket is an endpoint(at an ip on a port) that sends and receives data
+# Initialize socket with socket family type AF_INET -> IPV4 & SOCK_STREAM -> streaming socket
 s = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
 
-# Bind the host and the port addr to the socket
-s.bind((soc.gethostname(), 1234))
-
-
-s.listen(5)   # the arg 5 decides the queue, in case of heavy load
+# Bind the host and the port to the socket
+s.bind((soc.gethostname(), 3200))
+s.listen(5)    # the arg 5 defines the queue, in case oif heavy loads
 
 while True:
     client_socket, address = s.accept()
-    print(f"Conn. from {address} is established!!!")
-    client_socket.send(bytes("Welcome to the server!", "utf-8"))
-    client_socket.close()
+    print(f"Conn. from {address} has been established!")
+
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    
+    client_socket.send(bytes(msg, "utf-8"))
+    
+    while True:
+        time.sleep(5)
+
+        msg = f"The time is {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+        client_socket.send(bytes(msg, "utf-8"))
