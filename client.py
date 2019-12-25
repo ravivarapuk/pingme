50eb16e120f460f72ab6beb37038874df01dcc0b
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-25 09:13:38 +0530

    client

--- client.py
+++ client.py
@@ -1,3 +1,46 @@
 # Client 
 import socket as sc
 import select
+import errno
+import sys
+
+
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 8081
+
+my_username = input("username: ")
+client_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode("utf-8")
+username_header = f"{len(username) :< {HEADER_LENGTH}}".encode("utf-8")
+client_socket.send(username_header + username)
+
+while True:
+    message = input(f"{my_username} > ")
+
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+
+        client_socket.send(message_header + message)
+
+
+    while True:
+        # to recv things until we hit any error
+        username_header = client_socket.recv(HEADER_LENGTH)
+        if not len(username_header):
+            print("Connection closed by server!")
+            sys.exit()
+            
+        username_length = int(username_header.decode("utf-8").strip())
+        username = client_socket.recv(username_length).decode("utf-8")
+
+        message_header = client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode("utf-8").strip())
+        message = client_socket.recv(message_length).decode("utf-8")
+
+        print(f"{username} > {message}")
