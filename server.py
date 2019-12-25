b3f0f02f9859cf596e9f3b50374ae9a577a9df20
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-25 08:25:36 +0530

    creating the server (prod

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
 import socket as sc
-import select
+import select # This gives os level capabilities
+
 
 HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 
+
 server_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
-server_socket.setsockopt(sc.SOL_SOCKET, sc.SO_REUSEADDR, 1)
+server_socket.setsockopt(sc.SOL_SOCKET, sc.SO_REUSEADDR, 1) 
+
 
 server_socket.bind((IP, PORT))
-
 server_socket.listen()
 
-sockets_list = [server_socket]
+sockets_list = [server_socket] # This will handle the list of clients 
 
 clients = {}
 
@@ -29,22 +31,5 @@ def rec_message(client_socket):
 
         return {"header": message_header, "data": client_socket.recv(message_length)}
 
-
     except:
         return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
