a05bc0d7790ebda2b8d613e9601aa5c088f2ad27
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-25 08:37:32 +0530

    server enhancement
    adding more dynamic functionality

--- server.py
+++ server.py
@@ -16,11 +16,14 @@ server_socket.listen()
 
 sockets_list = [server_socket] # This will handle the list of clients 
 
-clients = {}
+clients = {} # PII with respect to the clients that are connected
 
 
 
 def rec_message(client_socket):
+    """
+    Utility to recv messages
+    """
     try:
         message_header = client_socket.recv(HEADER_LENGTH)
 
@@ -28,8 +31,26 @@ def rec_message(client_socket):
             return False
 
         message_length = int(message_header.decode("utf-8").strip())
-
         return {"header": message_header, "data": client_socket.recv(message_length)}
 
     except:
         return False
+
+
+while True:
+    # Select takes three params as lists which are the read_list, write_list and error on sockets
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_addr = server_socket.accept()
+
+            user = rec_message(client_socket)
+            if user is False:
+                continue
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print(f"Accepted new connection from {client_addr[0]}.{client_addr[1]} username: {user['data'].decode('utf-8')}")
