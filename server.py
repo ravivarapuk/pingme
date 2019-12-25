9e4984d67ecb8fcd28502d8647471e361abb8911
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2019-12-25 08:46:15 +0530

    final server

--- server.py
+++ server.py
@@ -54,3 +54,24 @@ while True:
             clients[client_socket] = user
 
             print(f"Accepted new connection from {client_addr[0]}.{client_addr[1]} username: {user['data'].decode('utf-8')}")
+
+        else:
+            message = rec_message(notified_socket)
+
+            if message is False:
+                print(f"Closed conn from {clients[notified_socket]['data'].decode('utf-8')}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+
+            user = clients[notified_socket]
+            print(f"Receiveed msg from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
+        
